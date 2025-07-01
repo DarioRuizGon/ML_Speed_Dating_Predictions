c1cb70a2ef2d0fdb7267e41b3a05d8e6ada244d9
DarioRuizGon <darioruiz334@gmail.com>
2025-07-01 18:01:24 +0200

    avances 01-07

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,13 +1,43 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+import re
+import imblearn.pipeline as imbpipe
+import sklearn.pipeline as skpipe
+
+import sys
+sys.path.insert(0, "../utils")
+import bootcampviztools as viz
+import ToolBox as tb
+import functions as fn
+
+np.random.seed(42)
+
+from collections import Counter
+
+from scipy.io import arff # Necesario para leer los datos, ya que vienen en este formato
+from scipy.stats import f_oneway, pearsonr, spearmanr
+
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE, RFECV, SelectFromModel, SelectKBest, f_classif, mutual_info_classif, chi2
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mutual_info_score
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+
+from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
+
 
 def feature_selection_from_model(X, y, n_folds, n_features_to_select, scoring, threshold = "median", estimator = None):
     if not estimator:
         estimator = RandomForestClassifier(max_depth= 8, random_state=42, class_weight= "balanced")
 
     cv = KFold(n_splits= n_folds, shuffle = True, random_state= 42)
-
+    # Selectores basados en estimador
     model_selector = SelectFromModel(estimator= estimator, threshold= threshold, max_features= n_features_to_select)
     rfe = RFE(estimator= estimator, n_features_to_select= n_features_to_select, step=1)
     rfecv = RFECV(estimator= estimator, cv= cv, scoring = scoring)
@@ -15,11 +45,33 @@ def feature_selection_from_model(X, y, n_folds, n_features_to_select, scoring, t
     for selector in [model_selector, rfe, rfecv]:
         selector.fit(X, y)
 
-    features_model = model_selector.get_feature_names_out().tolist()
-    features_rfe = rfe.get_feature_names_out().tolist()
-    features_rfecv = rfecv.get_feature_names_out().tolist()
+    model_selection = model_selector.get_feature_names_out().tolist()
+    rfe_selection = rfe.get_feature_names_out().tolist()
+    rfecv_selection = rfecv.get_feature_names_out().tolist()
 
-    return (features_model, features_rfe, features_rfecv)
+    return (model_selection, rfe_selection, rfecv_selection)
+
+
+
+def hard_voting(list):
+    voting = Counter(list) # Cuento cuántas veces ha salido cada una
+    df_voting = pd.DataFrame(voting.values(), columns = ["votes"], index = voting.keys()).sort_values("votes", ascending=False)
+    return df_voting
+
+
+
+def kbest_selector(X, y, k_anova = 15, k_mutual_info = 15, num_vars = None, cat_vars = None):
+    if num_vars:
+        anova_selector = SelectKBest(f_classif, k = k_anova)
+        anova_selector.fit(X.dropna()[num_vars], y.dropna())
+    
+    if cat_vars:
+        mutual_info_selector = SelectKBest(mutual_info_classif, k = k_mutual_info)
+        mutual_info_selector.fit(X.dropna()[cat_vars], y.dropna())
+
+    kbest_selection = anova_selector.get_feature_names_out().tolist() + mutual_info_selector.get_feature_names_out().tolist()
+
+    return kbest_selection
 
 
 
@@ -30,7 +82,7 @@ def feature_set_selection(X, y, n_folds, scoring, set_names, sets, model_names =
     if not models:
         dtc = DecisionTreeClassifier(max_depth= 8, random_state=42, class_weight= "balanced")
         rfc = RandomForestClassifier(max_depth=8, random_state=42, class_weight= "balanced")
-        xgbc = XGBClassifier(max_depth=8, random_state=42, scale_pos_weight = len(y_train[y_train == 0]) / len(y_train[y_train == 1]))
+        xgbc = XGBClassifier(max_depth=8, random_state=42, scale_pos_weight = len(y[y == 0]) / len(y[y == 1]))
         lgbmc = LGBMClassifier(max_depth=8, random_state=42, verbose = -1, class_weight= "balanced")
         models = [dtc, rfc, xgbc, lgbmc]
     
